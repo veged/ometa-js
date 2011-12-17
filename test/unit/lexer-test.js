2835b66127f1d41a790cf420d18da8663b274a8b
Fedor Indutny <fedor.indutny@gmail.com>
2011-12-18 01:29:07 +0600

    [lexer] parse 'strings' with double quotes

--- test/unit/lexer-test.js
+++ test/unit/lexer-test.js
@@ -8,19 +8,19 @@ exports['lexer test'] = function(test) {
     { type: 'string', value: 'abc', offset: 4 },
     { type: 'name', value: 'c', offset: 7 },
     { type: 'space', value: ' ', offset: 8 },
-    { type: 'string', value: 'abc', offset: 9 },
-    { type: 'space', value: ' ', offset: 14 },
-    { type: 'token', value: 'abc', offset: 15 },
-    { type: 'space', value: ' ', offset: 20 },
-    { type: 'punc', value: '{', offset: 21 },
-    { type: 'punc', value: '}', offset: 22 },
+    { type: 'string', value: '"a""bc', offset: 9 },
+    { type: 'space', value: ' ', offset: 17 },
+    { type: 'token', value: 'abc', offset: 18 },
     { type: 'space', value: ' ', offset: 23 },
-    { type: 'punc', value: '[', offset: 24 },
-    { type: 'string', value: 'a', offset: 25 },
-    { type: 'name', value: 'a', offset: 26 },
-    { type: 'space', value: ' ', offset: 27 },
-    { type: 'punc', value: ']', offset: 28 }
-  ], common.lexems('abc `abc \'abc\' "abc" {} [#a ]'));
+    { type: 'punc', value: '{', offset: 24 },
+    { type: 'punc', value: '}', offset: 25 },
+    { type: 'space', value: ' ', offset: 26 },
+    { type: 'punc', value: '[', offset: 27 },
+    { type: 'string', value: 'a', offset: 28 },
+    { type: 'name', value: 'a', offset: 29 },
+    { type: 'space', value: ' ', offset: 30 },
+    { type: 'punc', value: ']', offset: 31 }
+  ], common.lexems('abc `abc \'"a""bc\' "abc" {} [#a ]'));
 
   test.done();
 };
