235a1b42236489fdd0f42ac9aaa88c5a677a05da
Fedor Indutny <fedor.indutny@gmail.com>
2011-12-18 20:25:29 +0600

    [lexer] fix strings and tokens matching

--- test/unit/lexer-test.js
+++ test/unit/lexer-test.js
@@ -7,7 +7,7 @@ exports['lexer test'] = function(test) {
     { type: 'space', value: ' ', offset: 3 },
     { type: 'string', value: 'abc', offset: 4 },
     { type: 'space', value: ' ', offset: 8 },
-    { type: 'string', value: '"a""bc', offset: 9 },
+    { type: 'string', value: '"a""\'', offset: 9 },
     { type: 'space', value: ' ', offset: 17 },
     { type: 'token', value: 'abc', offset: 18 },
     { type: 'space', value: ' ', offset: 23 },
@@ -18,7 +18,7 @@ exports['lexer test'] = function(test) {
     { type: 'string', value: 'a', offset: 28 },
     { type: 'space', value: ' ', offset: 30 },
     { type: 'punc', value: ']', offset: 31 }
-  ], common.lexems('abc `abc \'"a""bc\' "abc" {} [#a ]'));
+  ], common.lexems('abc `abc \'"a""\\\'\' "abc" {} [#a ]'));
 
   test.done();
 };
