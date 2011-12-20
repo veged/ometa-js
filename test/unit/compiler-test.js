ff4324aa0d2cfafee79a3a404feb1ff24b2733e8
Fedor Indutny <fedor.indutny@gmail.com>
2011-12-20 11:11:36 +0600

    [compiler] IR fixes
    * core: added .anything(), added .simulate()
    * tests: added basic compiler tests

---
+++ test/unit/compiler-test.js
@@ -0,0 +1,103 @@
+var common = require('../fixtures/common'),
+    assert = require('assert');
+
+exports['empty grammar without parent'] = function(test) {
+  var code = common.compile('ometa G {}');
+  assert.equal(
+    code,
+    'function G(source) {AbstractGrammar.call(this, source);};' +
+    'require("util").inherits(G,AbstractGrammar);'
+  );
+  test.done();
+};
+
+exports['empty grammar with parent'] = function(test) {
+  var code = common.compile('ometa G <: P {}');
+  assert.equal(
+    code,
+    'function G(source) {P.call(this, source);};' +
+    'require("util").inherits(G,P);'
+  );
+  test.done();
+};
+
+exports['grammar with one empty rule'] = function(test) {
+  var code = common.compile('ometa G <: P { a }');
+  assert.equal(
+    code,
+    'function G(source) {P.call(this, source);};' +
+    'require("util").inherits(G,P);' +
+    'G.prototype._rule_a = function _rule_a() {return };'
+  );
+  test.done();
+};
+
+exports['grammar with two empty rules'] = function(test) {
+  var code = common.compile('ometa G <: P { a, b }');
+  assert.equal(
+    code,
+    'function G(source) {P.call(this, source);};' +
+    'require("util").inherits(G,P);' +
+    'G.prototype._rule_a = function _rule_a() {return };' +
+    'G.prototype._rule_b = function _rule_b() {return };'
+  );
+  test.done();
+};
+
+exports['grammar with rule with a match'] = function(test) {
+  var code = common.compile('ometa G <: P { a :b }');
+  assert.equal(
+    code,
+    'function G(source) {P.call(this, source);};' +
+    'require("util").inherits(G,P);' +
+    'G.prototype._rule_a = function _rule_a() {' +
+    'return this._rule_anything() && this.set("b")' +
+    '};'
+  );
+  test.done();
+};
+
+exports['grammar with rule with two matches'] = function(test) {
+  var code = common.compile('ometa G <: P { a :b :c }');
+  assert.equal(
+    code,
+    'function G(source) {P.call(this, source);};' +
+    'require("util").inherits(G,P);' +
+    'G.prototype._rule_a = function _rule_a() {' +
+    'return this._rule_anything() && this.set("b") && ' +
+    'this._rule_anything() && this.set("c")' +
+    '};'
+  );
+  test.done();
+};
+
+exports['grammar with rule with two choices'] = function(test) {
+  var code = common.compile('ometa G <: P { a = (:b | :c) }');
+  assert.equal(
+    code,
+    'function G(source) {P.call(this, source);};' +
+    'require("util").inherits(G,P);' +
+    'G.prototype._rule_a = function _rule_a() {' +
+    'return ' +
+    '(this._rule_anything() && this.set("b")) || ' +
+    '(this._rule_anything() && this.set("c"))' +
+    '};'
+  );
+  test.done();
+};
+
+exports['grammar with rule with two choices and predicate'] = function(test) {
+  var code = common.compile('ometa G <: P { a = :p (:b | :c) }');
+  assert.equal(
+    code,
+    'function G(source) {P.call(this, source);};' +
+    'require("util").inherits(G,P);' +
+    'G.prototype._rule_a = function _rule_a() {' +
+    'return ' +
+    'this._rule_anything() && this.set("p") && ' +
+    '((this._rule_anything() && this.set("b")) || ' +
+    '(this._rule_anything() && this.set("c")))' +
+    '};'
+  );
+  test.done();
+};
