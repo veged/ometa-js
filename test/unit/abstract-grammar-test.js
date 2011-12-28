e13db8db3fb94f3c6ce423e31f1cd46c61a3924b
Fedor Indutny <fedor.indutny@gmail.com>
2011-12-28 20:00:27 +0600

    [testes] new tests (mocha)

--- test/unit/abstract-grammar-test.js
+++ test/unit/abstract-grammar-test.js
@@ -1,125 +1,143 @@
 var common = require('../fixtures/common'),
     assert = require('assert');
 
-exports['`any`: number in string'] = function(test) {
-  function check(source, value) {
-    var g = common.ag(source);
+suite('AbstractGrammar class', function() {
+  suite('.any() method given digit as body should work', function() {
+    function check(source, value) {
+      var g = common.ag(source);
 
-    assert.ok(g.cache('g', 'rule', function() {
-      return this.any(function() {
-        return this.match(function(v) {
-          return '0' <= v && v <= '9';
-        });
-      });
-    }));
-
-    assert.deepEqual(g.intermediate, value);
-  };
-
-  check('123a', ['1', '2', '3']);
-  check('1', ['1']);
-  check('abc', []);
-
-  test.done();
-};
-
-exports['`many` : number in string'] = function(test) {
-  function check(source, value, fail) {
-    var g = common.ag(source),
-        start = g.cache('g', 'rule', function() {
-          return this.many(function() {
-            return this.match(function(v) {
-              return '0' <= v && v <= '9';
-            });
+      assert.ok(g.cache('g', 'rule', function() {
+        return this.any(function() {
+          return this.match(function(v) {
+            return '0' <= v && v <= '9';
           });
         });
+      }));
 
-    if (fail) {
-      assert.ok(!start);
-    } else {
-      assert.ok(start);
       assert.deepEqual(g.intermediate, value);
-    }
-  };
+    };
 
-  check('123a', ['1', '2', '3']);
-  check('1', ['1']);
-  check('abc', null, true);
+    test('on `123a` string', function() {
+      check('123a', ['1', '2', '3']);
+    });
 
-  test.done();
-};
+    test('on `1` string', function() {
+      check('1', ['1']);
+    });
 
-exports['`optional`: $ in string'] = function(test) {
-  function check(source, value, fail) {
-    var g = common.ag(source);
+    test('even on `abc` string', function() {
+      check('abc', []);
+    });
+  });
 
-    assert.ok(
-      g.cache('g', 'rule', function() {
-        return this.optional(function() {
-          return this.match('$');
+  suite('.many() method given digit as body should', function() {
+    function check(source, value, fail) {
+      var g = common.ag(source),
+          start = g.cache('g', 'rule', function() {
+            return this.many(function() {
+              return this.match(function(v) {
+                return '0' <= v && v <= '9';
+              });
+            });
+          });
+
+      if (fail) {
+        assert.ok(!start);
+      } else {
+        assert.ok(start);
+        assert.deepEqual(g.intermediate, value);
+      }
+    };
+
+    test('work on `123a` string', function() {
+      check('123a', ['1', '2', '3']);
+    });
+
+    test('work on `1` string', function() {
+      check('1', ['1']);
+    });
+
+    test('fail on `abc` string', function() {
+      check('abc', null, true);
+    });
+
+  });
+
+  suite('.optional() method given `$` match as body should', function() {
+    function check(source, value, fail) {
+      var g = common.ag(source);
+
+      assert.ok(
+        g.cache('g', 'rule', function() {
+          return this.optional(function() {
+            return this.match('$');
+          })
         })
-      })
-    );
-    assert.deepEqual(g.intermediate, value);
-  };
+      );
+      assert.deepEqual(g.intermediate, value);
+    };
 
-  check('$a', '$');
-  check('a', undefined);
+    test('work on `$a` string', function() {
+      check('$a', '$');
+    });
 
-  test.done();
-};
+    test('fail on `a` string', function() {
+      check('a', undefined);
+    });
+  });
 
-exports['token rule'] = function(test) {
-  var g = common.ag('token1     token2');
+  suite('`token` rule', function() {
+    test('should match token and spaces in string', function() {
+      var g = common.ag('token1     token2');
 
-  assert.ok(
-    g.cache('g', 'rule', function() {
-      return this.simulate([function() { return 'token1' }], function() {
-        return this.rule('token');
-      }) &&
-      this.simulate([function() { return 'token2' }], function() {
-        return this.rule('token');
-      })
-    })
-  );
+      assert.ok(
+        g.cache('g', 'rule', function() {
+          return this.simulate([function() { return 'token1' }], function() {
+            return this.rule('token');
+          }) &&
+          this.simulate([function() { return 'token2' }], function() {
+            return this.rule('token');
+          })
+        })
+      );
+    });
+  });
 
-  test.done();
-};
+  suite('`fromTo` rule', function() {
+    test('should match chars between `from` and `to` in string', function() {
+      var g = common.ag('a/* xyz */b');
 
-exports['fromTo rule'] = function(test) {
-  var g = common.ag('a/* xyz */b');
+      assert.ok(
+        g.cache('g', 'rule', function() {
+          return this.match('a') &&
+                 this.atomic(function() {
+                   return this.simulate([
+                     function() { return '/*' },
+                     function() { return '*/' }
+                   ], function() {
+                     return this.rule('fromTo');
+                   })
+                 }) && this.store(function(v) {
+                   assert.equal(v, '/* xyz */');
+                 }) && this.match('b');
+        })
+      );
+    });
+  });
 
-  assert.ok(
-    g.cache('g', 'rule', function() {
-      return this.match('a') &&
-             this.atomic(function() {
-               return this.simulate([
-                 function() { return '/*' },
-                 function() { return '*/' }
-               ], function() {
-                 return this.rule('fromTo');
-               })
-             }) && this.store(function(v) {
-               assert.equal(v, '/* xyz */');
-             }) && this.match('b');
-    })
-  );
+  suite('`seq` rule', function() {
+    test('should match sequence of chars in string', function() {
+      var g = common.ag('abcd');
 
-  test.done();
-};
-
-exports['seq rule'] = function(test) {
-  var g = common.ag('abcd');
-
-  assert.ok(
-    g.cache('g', 'rule', function() {
-      return this.match('a') &&
-             this.rule('seq', [function() { return 'bcd' }]) &&
-             this.store(function(v) {
-               assert.equal(v, 'bcd');
-             });
-    })
-  );
-
-  test.done();
-};
+      assert.ok(
+        g.cache('g', 'rule', function() {
+          return this.match('a') &&
+                 this.rule('seq', [function() { return 'bcd' }]) &&
+                 this.store(function(v) {
+                   assert.equal(v, 'bcd');
+                 });
+        })
+      );
+    });
+  });
+});
