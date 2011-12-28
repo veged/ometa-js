1fb8dc81e91a69b04fc8785e20459787ad052671
Fedor Indutny <fedor.indutny@gmail.com>
2011-12-28 20:19:53 +0600

    [test] functional test

---
+++ test/unit/functional-test.js
@@ -0,0 +1,64 @@
+var common = require('../fixtures/common'),
+    assert = require('assert');
+
+suite('Ometajs module', function() {
+  function unit(grmr, rule, src, dst) {
+    var i = new grmr(src);
+
+    assert.ok(i.rule(rule));
+    assert.deepEqual(i.intermediate, dst);
+  };
+
+  suite('given a simple left recursion grammar', function() {
+    var grmr = common.require('lr').LeftRecursion;
+
+    test('should match input successfully', function() {
+      unit(
+        grmr,
+        'expr',
+        '123 + 456 - 789.4',
+        [
+          '-' ,
+          [ '+', [ 'number', 123 ], [ 'number', 456 ] ],
+          [ 'number', 789.4 ]
+        ]
+      );
+    });
+  });
+
+  suite('given a javascript grammar', function() {
+    var grmr = common.require('bs-js-compiler').BSJSParser;
+
+    function js(code, ast) {
+      test('`'+ code + '`', function() {
+        unit(grmr, 'topLevel', code, ast);
+      });
+    }
+
+    suite('should match', function() {
+      js('var x', ['begin', ['var', ['x']]]);
+      js('var x = 1', ['begin', ['var', ['x', ['number', 1]]]]);
+      js('var x = 1, y, z;', ['begin',
+         ['var', ['x', ['number', 1]], ['y'], ['z']]
+      ]);
+
+      js('function a() {}', ['begin',
+         ['var', ['a', ['func', [], ['begin']]]]
+      ]);
+
+      js('function a() {return a()}', ['begin',
+         ['var', ['a', ['func', [], ['begin', [
+           'return', ['call', ['get', 'a']]
+         ]]]]]
+      ]);
+
+      js('function a() {return a()};"123"+"456"', ['begin',
+         ['var', ['a', ['func', [], [
+           'begin', ['return', ['call', ['get', 'a']]]
+         ]]]],
+         ['get', 'undefined'],
+         ['binop', '+', ['string', '123'], ['string', '456']]
+      ]);
+    });
+  });
+});
