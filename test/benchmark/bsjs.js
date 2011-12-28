8acebbd5527807fd7609749af6169ae9be337587
Fedor Indutny <fedor.indutny@gmail.com>
2011-12-29 05:07:28 +0600

    [benchmark]

---
+++ test/benchmark/bsjs.js
@@ -0,0 +1,13 @@
+var common = require('../fixtures/common'),
+    assert = require('assert');
+
+var n = 1000,
+    source  = new Array(n).join('if (x) { return { x:3, y:4, z:5} };');
+console.log('Source length: %d', source.length);
+
+var grmr = common.require('bs-js-compiler').BSJSParser,
+    instance = new grmr(source);
+
+console.time('parse');
+instance.rule('topLevel');
+console.timeEnd('parse');
