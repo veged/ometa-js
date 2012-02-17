6bc41d413d4ae08827a86d9dbdc73230a7eb622c
Fedor Indutny <fedor.indutny@gmail.com>
2012-02-17 21:27:39 +0600

    grammar: less caching, better performance
    * test: more real world benchmark

--- test/benchmark/bsjs.js
+++ test/benchmark/bsjs.js
@@ -2,7 +2,17 @@ var common = require('../fixtures/common'),
     assert = require('assert');
 
 var n = 1000,
-    source  = new Array(n).join('if (x) { return { x:3, y:4, z:5} };');
+    source  = new Array(n).join(
+      'if (x) {' +
+      '   return function() {' +
+      '        var x = y;' +
+      '        y += x;' +
+      '        return { x:3, y:4 + 6, z:5};' +
+      '   };' +
+      '   return function() {' +
+      '        return { x:3, y:4, z:5};' +
+      '   }' +
+      '};');
 console.log('Source length: %d', source.length);
 
 var grmr = common.require('bs-js-compiler').BSJSParser,
