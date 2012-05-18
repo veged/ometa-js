e428bb4de8121896728e19369c61a0fa0744df7a
Fedor Indutny <fedor.indutny@gmail.com>
2012-05-18 18:32:18 +0700

    test: compiler tests

--- test/unit/functional-test.js
+++ test/unit/functional-test.js
@@ -26,47 +26,78 @@ suite('Ometajs module', function() {
     });
   });
 
-  suite('given a javascript grammar', function() {
-    var grmr = common.require('bs-js-compiler').BSJSParser;
+  suite('given a javascript grammar\'s', function() {
+    suite('parser', function() {
+      var grmr = common.require('bs-js-compiler').BSJSParser;
 
-    function js(code, ast) {
-      var name;
-      if (code.length > 50) {
-        name = code.slice(0, 47) + '...';
-      } else {
-        name = code;
+      function js(code, ast) {
+        var name;
+        if (code.length > 50) {
+          name = code.slice(0, 47) + '...';
+        } else {
+          name = code;
+        }
+
+        test('`'+ name + '`', function() {
+          unit(grmr, 'topLevel', code, ast);
+        });
       }
 
-      test('`'+ name + '`', function() {
-        unit(grmr, 'topLevel', code, ast);
+      suite('should match', function() {
+        js('var x', ['begin', ['var', ['x']]]);
+        js('var x = 1.2', ['begin', ['var', ['x', ['number', 1.2]]]]);
+        js('var x = 1e2, y, z;', ['begin',
+           ['var', ['x', ['number', 100]], ['y'], ['z']]
+        ]);
+        js('x.y', [ 'begin', [ 'getp', [ 'string', 'y' ], [ 'get' , 'x' ] ] ]);
+
+        js('function a() {}', ['begin',
+           ['var', ['a', ['func', [], ['begin']]]]
+        ]);
+
+        js('function a() {return a()}', ['begin',
+           ['var', ['a', ['func', [], ['begin', [
+             'return', ['call', ['get', 'a']]
+           ]]]]]
+        ]);
+
+        js('function a() {return a()};"123"+"456"', ['begin',
+           ['var', ['a', ['func', [], [
+             'begin', ['return', ['call', ['get', 'a']]]
+           ]]]],
+           ['get', 'undefined'],
+           ['binop', '+', ['string', '123'], ['string', '456']]
+        ]);
       });
-    }
+    });
 
-    suite('should match', function() {
-      js('var x', ['begin', ['var', ['x']]]);
-      js('var x = 1.2', ['begin', ['var', ['x', ['number', 1.2]]]]);
-      js('var x = 1e2, y, z;', ['begin',
-         ['var', ['x', ['number', 100]], ['y'], ['z']]
-      ]);
-      js('x.y', [ 'begin', [ 'getp', [ 'string', 'y' ], [ 'get' , 'x' ] ] ]);
+    suite('compiler', function() {
+      var grmr = common.require('bs-js-compiler').BSJSTranslator;
 
-      js('function a() {}', ['begin',
-         ['var', ['a', ['func', [], ['begin']]]]
-      ]);
+      function unit(name, ast, source) {
+        test(name, function() {
+          assert.equal(grmr.match(ast, 'trans'), source);
+        });
+      }
 
-      js('function a() {return a()}', ['begin',
-         ['var', ['a', ['func', [], ['begin', [
-           'return', ['call', ['get', 'a']]
-         ]]]]]
-      ]);
-
-      js('function a() {return a()};"123"+"456"', ['begin',
-         ['var', ['a', ['func', [], [
-           'begin', ['return', ['call', ['get', 'a']]]
-         ]]]],
-         ['get', 'undefined'],
-         ['binop', '+', ['string', '123'], ['string', '456']]
-      ]);
+      unit('undefined', ['get', 'undefined'], 'undefined');
+      unit('basic name', ['get', 'a'], 'a');
+      unit('var declaration', ['var', ['x', ['number', 1]]], 'var x = (1)');
+      unit(
+        'block with statements',
+        ['begin', ['get', 'x'], ['var', ['x', ['number', 1]]]],
+        '{x;var x = (1)}'
+      );
+      unit(
+        'binop',
+        ['binop', '+', ['get', 'x'], ['get', 'y']],
+        '(x + y)'
+      );
+      unit(
+        'complex assignment',
+        ['set', ['getp', ['get', 'x'], ['get', 'y']], ['get', 'z']],
+        '(y[x]=z)'
+      );
     });
   });
 });
