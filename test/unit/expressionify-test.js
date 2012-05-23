dbf07b2829baa03b749adc19c3bef3960e659bf0
Fedor Indutny <fedor.indutny@gmail.com>
2012-05-23 16:39:05 +0400

    utils: wrap in function before expressionifying
    * bsjs: fix unescaping strings

--- test/unit/expressionify-test.js
+++ test/unit/expressionify-test.js
@@ -11,6 +11,7 @@ suite('utils.expressionify', function() {
     unit('a;b', 'function(){a;return b}.call(this)');
     unit('{ x = 1 }', 'function(){return x=1}.call(this)');
     unit('{ x = 1; y = 2 }', 'function(){x=1;return y=2}.call(this)');
+    unit('switch (x) { case 1: return x; }', 'switch(x){case 1:return x}');
   });
 
 });
