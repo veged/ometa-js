408088e34ea965592ff997ec1d4085053f927533
Fedor Indutny <fedor.indutny@gmail.com>
2012-07-12 22:04:43 +0400

    bsjs: fix keyword-property lookup

--- test/unit/functional-test.js
+++ test/unit/functional-test.js
@@ -180,6 +180,11 @@ suite('Ometajs module', function() {
         'a["a-b"]'
       );
       unit(
+        'property lookup (regr#2)',
+        ['getp', ['string', 'for'], ['get', 'a']],
+        'a["for"]'
+      );
+      unit(
         'typeof plus dot',
         ['getp', ['string', 'b'], ['unop', 'typeof', ['get', 'a']]],
         '(typeof a).b'
